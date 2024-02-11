b69563c414987a767978ae5d728a56987d2322a5
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:20:21 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'VORP'
+author 'VORP @nolosharp'
 description 'A script to handle ambient peds and vehicles traffic'
 repository 'https://github.com/VORPCORE/vorp_traffic'
 
