a6c10bb4a77e232661af4a6d5902b4bd9e563a3d
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:17:45 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,15 @@
-fx_version "adamant"
-
-games { 'rdr3', 'gta5' }
-
+fx_version 'cerulean'
+game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_script {
-	'traffic.lua',
-}
+author 'VORP'
+description 'A script to handle ambient peds and vehicles traffic'
+repository 'https://github.com/VORPCORE/vorp_traffic'
+
+client_script 'traffic.lua'
+
+--dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_traffic'
